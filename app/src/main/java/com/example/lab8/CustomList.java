16b9ef5f4b63980ae4dc4f79a8d067af50879b53
kidus-der <101529458+kidus-der@users.noreply.github.com>
2024-03-08 15:36:26 -0700

    lablablab8

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -57,12 +57,15 @@ public class CustomList extends ArrayAdapter<City> {
         return cities.contains(city);
     }
 
+    /*
     public void delete(City city) {
         if (!cities.contains(city)) {
             throw new IllegalArgumentException("City does not exist in the list");
         }
         cities.remove(city);
     }
+    
+     */
 
     public int countCities() {
         return cities.size();
