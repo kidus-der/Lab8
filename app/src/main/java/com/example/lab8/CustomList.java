cd8ddab05f62d5da146f6d09bc4d5b449b75b95a
kidus-der <101529458+kidus-der@users.noreply.github.com>
2024-03-08 15:40:28 -0700

    added hasCity

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -53,12 +53,10 @@ public class CustomList extends ArrayAdapter<City> {
         cities.add(city);
     }
 
-    /*
+
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
-    
-     */
 
 
     public void delete(City city) {
